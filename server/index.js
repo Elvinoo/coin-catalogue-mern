e5c44c2dcfbd94c60a6699f79dfe8b222122a6fc
Elvinoo <elvinooo1990@gmail.com>
2023-08-09 23:37:02 +0400

    app.use deleted

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 const PORT = process.env.PORT || 5000;
 mongoose.set("strictQuery", false);
-app.use('/images', express.static(__dirname + '/public/images'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
